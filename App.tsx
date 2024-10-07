aa2c7f188976e2c09b8476eb7dc377950aa07b92
ST10441309 <ST10441309@vcconnect.edu.za>
2024-10-07 20:31:39 +0200

    removing dependencies

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import {
   View,
   Text,
+  TextInput,
+  FlatList,
+  TouchableHighlight,
+  Alert,
   StyleSheet,
 } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+
 import { Picker } from '@react-native-picker/picker';
 
 const menuList = [
@@ -22,64 +25,6 @@ function Home() {
       <Text> MENU ITEMS </Text>
     </View>
   );  
-}
-function Login () {
-  return (
-    <View style={styles.container}>
-      <Text> PLEASE SIGN IN </Text>
-    </View>
-  );
-}
-function Enter () {
-  return (
-    <View style={styles.container}>
-      <Text> CHOOSE YOUR MEAL </Text>
-    </View>
-  );
-}
-function Clients () {
-  return (
-    <View style={styles.container}>
-      <Text></Text>
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
-
-function MyDrawer() {
-  return (
-    <Drawer.Navigator initialRouteName='Home'>
-      <Drawer.Screen
-        name='Chef Cristoffel: Home'
-        component={Home}
-        options={{ drawerLabel: 'Home'}}
-      />
-      <Drawer.Screen
-        name='Chef Cristoffel: Login Screen'
-        component={Login}
-        options={{ drawerLabel: 'Sign in'}}
-      />
-      <Drawer.Screen
-        name='Chef Cristoffel: Enter Items'
-        component={Enter}
-        options={{ drawerLabel: 'Enter menu items'}}
-      />
-      <Drawer.Screen
-        name='Chef Cristoffel: Client Details'
-        component={Clients}
-        options={{ drawerLabel: 'View client details'}}
-      />
-    </Drawer.Navigator>
-  );
-}
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyDrawer/>
-    </NavigationContainer>
-  );
 }
 
 const styles = StyleSheet.create({
